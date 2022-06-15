00f5d1fefc7df053c323211e99d419cd6d42df03
wsr <wsr@DESKTOP-9F2NA3E>
2022-06-15 15:11:55 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    abstract class Person
+
+    {
+
+        public abstract string Name { get; }
+        public abstract string Surname { get; }
+        public abstract string Patronymic { get; }
+        abstract public void Income();
+    }
+    }
+
+
---
+++ ConsoleApp4/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Student : Person
+    {
+        public override string Name => throw new NotImplementedException();
+
+        public override string Surname => throw new NotImplementedException();
+
+        public override string Patronymic => throw new NotImplementedException();
+
+        public override void Income()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
